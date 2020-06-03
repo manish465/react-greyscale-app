93882a716590c143405792697a6bcbe1f3a43fb8
unknown <ms5978307@gmail.com>
2020-06-03 10:13:43 -0700

    added header contents

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,19 @@
 import React from "react";
 
+const tab = ["HOME", "GALLERY", "BLOG", "PAGE", "SHOP", "ELEMENTS"];
+
 const Header = () => {
-    return <div>Header</div>;
+    return (
+        <header className='appBar'>
+            <div className='logo-conatiner'>FUEGO.</div>
+            <ul className='appBar-tabs'>
+                {tab.map((el, key) => (
+                    <li key={key}>{el}</li>
+                ))}
+            </ul>
+            <div className='menu-icon'>menu</div>
+        </header>
+    );
 };
 
 export default Header;
