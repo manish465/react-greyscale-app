233c4f667a5a13976370d1aac8b492816267930d
unknown <ms5978307@gmail.com>
2020-06-03 11:21:00 -0700

    added assets and impot logs and icon

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -7,15 +7,19 @@ const tab = ["HOME", "GALLERY", "BLOG", "PAGE", "SHOP", "ELEMENTS"];
 const Header = () => {
     return (
         <header className='appBar'>
-            <div className='logo-conatiner'>FUEGO.</div>
+            <div className='logo-conatiner'>
+                <img src={require("../../assets/LogoIcons.svg")} alt='logo' />
+            </div>
             <ul className='appBar-tabs'>
                 {tab.map((el, key) => (
                     <li className='link-tabs' key={key}>
-                        {el}
+                        <button>{el}</button>
                     </li>
                 ))}
             </ul>
-            <div className='menu-icon'>menu</div>
+            <div className='menu-icon'>
+                <img src={require("../../assets/MenueButton.svg")} alt='' />
+            </div>
         </header>
     );
 };
