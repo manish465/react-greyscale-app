5acf0dd306a49edb5f1cee5729c8be15387e7dc0
unknown <ms5978307@gmail.com>
2020-06-03 10:05:14 -0700

    added header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
 
+import Header from "./components/Header";
+
 function App() {
-    return <>hello world</>;
+    return (
+        <>
+            <Header />
+        </>
+    );
 }
 
 export default App;
