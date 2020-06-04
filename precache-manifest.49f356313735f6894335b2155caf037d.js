ac73d74c1ca242b5e6bd8bc913fa5faafae04bdc
unknown <ms5978307@gmail.com>
2020-06-03 23:20:12 -0700

    Updates

---
+++ precache-manifest.49f356313735f6894335b2155caf037d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec6816c7cf54b0e1f13282cba21f82d6",
+    "url": "/react-greyscale-app/index.html"
+  },
+  {
+    "revision": "24115279ddad4a7ebb1a",
+    "url": "/react-greyscale-app/static/css/main.00f6e177.chunk.css"
+  },
+  {
+    "revision": "ee1279a7fccfc93475b4",
+    "url": "/react-greyscale-app/static/js/2.f939aa73.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/react-greyscale-app/static/js/2.f939aa73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24115279ddad4a7ebb1a",
+    "url": "/react-greyscale-app/static/js/main.c0518e9c.chunk.js"
+  },
+  {
+    "revision": "7149c520535dfabfffe3",
+    "url": "/react-greyscale-app/static/js/runtime-main.212f6a86.js"
+  },
+  {
+    "revision": "70c91c20ea5378bfbc16aceca1abfdaf",
+    "url": "/react-greyscale-app/static/media/LogoIcons.70c91c20.svg"
+  },
+  {
+    "revision": "6eca82080f124a1a89b1e64e814ab7d5",
+    "url": "/react-greyscale-app/static/media/MenueButton.6eca8208.svg"
+  },
+  {
+    "revision": "0738243ce55fde8e39e9beb91e70e86b",
+    "url": "/react-greyscale-app/static/media/img1.0738243c.png"
+  },
+  {
+    "revision": "8252f29502aef54141e2df45f4519b1b",
+    "url": "/react-greyscale-app/static/media/img2.8252f295.png"
+  },
+  {
+    "revision": "c10b56633faad87c89e1ddc216d36c6c",
+    "url": "/react-greyscale-app/static/media/img3.c10b5663.png"
+  },
+  {
+    "revision": "baa749779abebeee82626ff42ad80850",
+    "url": "/react-greyscale-app/static/media/img4.baa74977.png"
+  },
+  {
+    "revision": "7c73de33db6962e82e6d31551883784e",
+    "url": "/react-greyscale-app/static/media/img5.7c73de33.png"
+  }
+]);
